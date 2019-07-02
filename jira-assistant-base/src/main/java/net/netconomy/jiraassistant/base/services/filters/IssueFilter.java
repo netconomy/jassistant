cf0b8a8bb4f7f1471429e43e94f6ec36182201da
Horst Gutmann <h.gutmann@netconomy.net>
2019-07-02 10:24:35 +0200

    JASSISTANT-129: Upgrade to Java 11

---
+++ jira-assistant-base/src/main/java/net/netconomy/jiraassistant/base/services/filters/IssueFilter.java
@@ -0,0 +1,20 @@
+/*********************************************************************
+ * The Initial Developer of the content of this file is NETCONOMY.
+ * All portions of the code written by NETCONOMY are property of
+ * NETCONOMY. All Rights Reserved.
+ *
+ * NETCONOMY Software & Consulting GmbH
+ * Hilmgasse 4, A-8010 Graz (Austria)
+ * FN 204360 f, Landesgericht fuer ZRS Graz
+ * Tel: +43 (316) 815 544
+ * Fax: +43 (316) 815544-99
+ * www.netconomy.net
+ *
+ * (c) 2019 by NETCONOMY Software & Consulting GmbH
+ *********************************************************************/
+
+package net.netconomy.jiraassistant.base.services.filters;
+
+public class IssueFilter {
+
+}
